878b8841310e6e48fa0dd8a742d1a9faa0cb73bf
animasana <animagig@gmail.com>
2024-02-08 15:58:45 +0900

    +Suspense at Home

---
+++ components/movies.tsx
@@ -0,0 +1,28 @@
+import Movie from "../components/movie";
+import styles from "../styles/home.module.css";
+import { API_URL } from "../app/constants";
+
+const getMovies = async () => {
+  //await new Promise((resolve) => setTimeout(resolve, 10_000));
+  const response = await fetch(API_URL);
+  return response.json();  
+}
+
+export default async function Movies() {
+  const movies = await getMovies()
+
+  return (
+    <div className={styles.container}>        
+      {
+        movies.map(movie => 
+          <Movie 
+            key={movie.id} 
+            id={movie.id} 
+            title={movie.title} 
+            poster_path={movie.poster_path} 
+          />
+        )
+      }      
+    </div>
+  );
+}
