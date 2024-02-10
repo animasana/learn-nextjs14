de455327813dc8923dbe75a9bef18248198bc37a
animasana <animagig@gmail.com>
2024-02-10 19:33:32 +0900

    providers.module.css changed

--- components/movie-providers.tsx
+++ components/movie-providers.tsx
@@ -1,24 +1,26 @@
-import Link from "next/link";
 import { getMovie, moviePosterExists } from "./movie-info";
 import ProviderCountry from "./provider-country";
 import { MOVIE_NOT_FOUND } from "../app/constants";
 import styles from "../styles/providers.module.css"
+import { BackButton } from "./movie-credits";
 
 export default async function MovieProviders({ id }: {id: string}) {
   const movie = await getMovie(id);
-  
+
   return (
-    <div className={styles.container}>
-      <img 
-        src={moviePosterExists(movie) ? MOVIE_NOT_FOUND : movie.poster_path} 
-        className={styles.poster} 
-        alt={movie.title} 
-      />      
-      <div className={styles.logo}>        
-        <Link prefetch href={`/movies/${id}`}>&larr; Back to the movie info</Link>        
-        <ProviderCountry id={id} country="KR" />
-        <ProviderCountry id={id} country="US" />        
+    <>
+      <BackButton id={id} />
+      <div className={styles.container}>
+        <img 
+          src={moviePosterExists(movie) ? MOVIE_NOT_FOUND : movie.poster_path} 
+          className={styles.poster} 
+          alt={movie.title} 
+        />      
+        <div className={styles.icons}>        
+          <ProviderCountry id={id} country="KR" />
+          <ProviderCountry id={id} country="US" />        
+        </div>
       </div>
-    </div>
+    </>
   );
 }
