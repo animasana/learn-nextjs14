3fc8048bb3d09f4d22b39de116336d1fb96fd7b2
animasana <animagig@gmail.com>
2024-02-14 20:10:59 +0900

    test

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -12,17 +12,15 @@ export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
   return (
     <div className={styles.container}>
-      {
-        videos.map(video => 
-          <iframe 
-            key={video.id} 
-            src={`https://youtube.com/embed/${video.key}`} 
-            title={video.name}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;"
-            allowFullScreen
-          />
-        )
-      }
+      {videos.map(video => 
+        <iframe 
+          key={video.id} 
+          src={`https://youtube.com/embed/${video.key}`} 
+          title={video.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;"
+          allowFullScreen
+        />
+      )}
     </div>
   );
 }
